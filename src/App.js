f62ba6a61179607da6de7ea7ac0420f39f570aed
TomasSantosz <tomassantos016@gmail.com>
2021-04-01 22:12:45 -0300

    add Global_styles :art:

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
-/* import GlobalStyle from './styles/global'; */
+import GlobalStyle from './styles/global';
 import Routes from './routes';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes />
-      {/* <GlobalStyle /> */}
+      <GlobalStyle />
     </BrowserRouter>
   );
 }
